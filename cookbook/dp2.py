3174d3f18c3a205eb2074c915d1bbfb883ff0003
Ralph Tang <r33tang@uwaterloo.ca>
2022-02-21 01:43:53 -0500

    Add solutions

--- cookbook/dp2.py
+++ cookbook/dp2.py
@@ -1,5 +1,6 @@
 # Hard DP questions.
 
+from typing import List
 import random
 
 
@@ -32,11 +33,47 @@ def longest_valid_parentheses(string: str) -> int:
     return max(A)
 
 
+def paint_fence(n: int, k: int) -> int:
+    # Set up state: last one repeated, last one unrepeated
+    num_repeated = 0
+    num_unrepeated = k
+
+    # Transition equation.
+    for idx in range(1, n):
+        last_num_unrepeated = num_unrepeated
+        num_unrepeated = (num_unrepeated + num_repeated) * (k - 1)
+        num_repeated = last_num_unrepeated
+    
+    return num_unrepeated + num_repeated
+
+
+def paint_house2(costs: List[List[int]]) -> int:
+    # Set up state: min cost of painting the last house with the kth color.
+    min_last_costs = costs[0]
+
+    # Transition equation.
+    for idx1 in range(1, len(costs)):
+        new_min_costs = []
+
+        for idx2 in range(len(costs[idx1])):
+            new_cost = costs[idx1][idx2]
+            new_cost += min(x for idx, x in enumerate(min_last_costs) if idx != idx2)
+            new_min_costs.append(new_cost)
+        
+        min_last_costs = new_min_costs
+    
+    return min(min_last_costs)
+
+
 def main():
     rand_str = ''.join('()'[random.randint(0, 1)] for _ in range(10))
 
     print(rand_str)
     print(longest_valid_parentheses(rand_str))
+
+    print(paint_fence(3, 3))
+
+    print(paint_house2([[1, 5, 3], [2, 9, 4]]))
 
 
 if __name__ == '__main__':
